59ffb80cfeaf41746cc679ca072411206c3a6459
Saiyeesh <saiyeesh7941@gmail.com>
2025-01-05 00:46:18 +0530

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import "./Navbar.css"
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
      <>
        <div className='main'>
           <div className='firstNav'>
             <h2>Portfilo</h2>
           </div>
-          <ul className='ulsNav'>
+          <div className="hamburger" onClick={toggleMenu}>
+              <div></div>
+              <div></div>
+              <div></div>
+          </div>
+          <ul className={isMenuOpen ? "nav-links active" : "nav-links"} >
             <Link to="/" className='linkLine'><li>About</li></Link>
             <Link to="/skills" className='linkLine'><li>Skills</li></Link>
             <Link to="/experience" className='linkLine'><li>Experience</li></Link>
