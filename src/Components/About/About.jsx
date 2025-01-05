d6add17828960dea3415902cb259f7c3bd949b67
Saiyeesh <saiyeesh7941@gmail.com>
2025-01-05 09:19:46 +0530

    changes done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,6 +2,7 @@ import "./About.css"
 import profile from "..//../assets/pric.jpg"
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import resume from "..//..//../public/Resume.pdf" 
 
 function About() {
   return (
@@ -13,7 +14,7 @@ function About() {
             <h2>Yellarm Saiyeesh</h2>
             <h5>I am a <span style={{color: 'rgb(133, 76, 230)'}}>Full Stack Developer</span></h5>
             <p>I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivery high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.</p>
-            <a href='' target='_blank'><button>Check Resume</button></a>
+            <a href={resume} target='_blank'><button>Check Resume</button></a>
           </div>
           <div className='secoundPart'>
             <img src={profile} alt='profile-img' className='profileImg'/>
