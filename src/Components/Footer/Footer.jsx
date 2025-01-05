9b574732d2f8bf2f9380af2ab674782920b34588
Saiyeesh <saiyeesh7941@gmail.com>
2025-01-05 08:58:26 +0530

    small changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css"
-import { Link } from 'react-router-dom';
+import { FaRegCopyright } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -11,13 +11,14 @@ function Footer() {
       <div className='footerBack'>
         <h2>Yellarm Saiyeesh</h2>
         <div className='footer-logos'>
-            <FaFacebook className='logos'/>
-            <FaInstagram className='logos'/>
+            <a href="https://www.facebook.com/" target="_blank"><FaFacebook className='logos'/></a>
+            <a href="https://www.instagram.com/?utm_source=pwa_homescreen&__pwa=1" target="_blank"><FaInstagram className='logos'/></a>
             <a href='https://www.linkedin.com/feed/' target='_blank'><FaLinkedin className='logos'/></a>
-            <FaTwitter className='logos'/>
+            <a href="" target="_blank"><FaTwitter className='logos'/></a>
         </div>
         <div className='footerLast'>
-          <p>@ 2024 Yellarm Saiyeesh. All Rights Reserved</p>
+          <FaRegCopyright />
+          <p> 2024 Yellarm Saiyeesh. All Rights Reserved</p>
         </div>
       </div>
     </>
